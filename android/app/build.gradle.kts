d0ef63006e727d1ace9d565f655add9f664f2845
Jonathan <joniwinter6@gmail.com>
2026-04-18 10:19:19 +0200

    correctiong name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.buget_flow"
+    namespace = "com.example.budget_flow"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.buget_flow"
+        applicationId = "com.example.budget_flow"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/buget_flow/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.buget_flow
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
